4fbee732b7e7964234ce55af498f57c03e1b9754
Mateus Martins Freitas <mateusmfreitas12@gmail.com>
2025-05-08 20:04:10 -0300

    Finaliza a api

--- app/Http/Controllers/ClickTrackerController.php
+++ app/Http/Controllers/ClickTrackerController.php
@@ -10,17 +10,26 @@ class ClickTrackerController extends Controller
 {
     public function store(Request $request){
       $clicks = $request->input('clicks');
-
+      $coordinates = [];
       if(!is_array($clicks)){
         return response()->json(['error' => 'Formato inválido'], 422);
       }
+      $contagemCliques = [];
+
+      foreach($clicks as $index => $click){
+        if(!isset($contagemCliques[$click['id']])){
+            $contagemCliques[$click['id']] = 0;
+        }
+        $coordinates[] = $click['coords'];
+        $contagemCliques[$click['id']]++;
+      }
 
       $data = [
-        'ip' => $request->input('ip'),
+        'ip' => 'calma',
         'navegador' => $request->input('navegador'),
         'timestamp' => now(),
-        'clicks' => $clicks,
-        'coordenadas' => $request->input('coordinates'),
+        'clicks' => $contagemCliques,
+        'coordenadas' => $coordinates,
       ];
 
       $response = Http::post('http://172.26.192.1:5984/clicks', $data);
